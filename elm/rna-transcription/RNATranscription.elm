e8fc07f6f778a84a6c9918065bf361e44524f8e3
Congwen Ma <lostaznfound@gmail.com>
2018-01-09 12:56:04 -0600

    rna transcription

--- elm/rna-transcription/RNATranscription.elm
+++ elm/rna-transcription/RNATranscription.elm
@@ -2,43 +2,49 @@ module RNATranscription exposing (..)
 
 import Dict
 
+
 -- NOTE: maybe this needs to produce an error
+
+
 rnaDictionary : Dict.Dict Char Char
 rnaDictionary =
     Dict.fromList
-        [ ( 'G'
-          , 'C'
-          )
-        , ( 'C'
-          , 'G'
-          )
-        , ( 'T'
-          , 'A'
-          )
-        , ( 'A'
-          , 'U'
-          )
+        [ ( 'G', 'C' )
+        , ( 'C', 'G' )
+        , ( 'T', 'A' )
+        , ( 'A', 'U' )
         ]
 
 
 convertToRNA : Char -> Char
 convertToRNA dna =
-    Maybe.withDefault dna <| Dict.get dna rnaDictionary
+    Maybe.withDefault 'X' (Dict.get dna rnaDictionary)
 
 
-toRNA : String -> Result error String
-toRNA str = Ok "fake"
--- toRNA dna =
---     let
---         dnaList =
---             String.split "" dna
+toRNA : String -> Result Char String
+toRNA dna =
+    let
+        dnaList : List Char
+        dnaList =
+            String.toList dna
 
---         maybeDna =
---             Just <| String.join "" (dnaList |>
---             String.map convertToRNA)
---     in
---         case maybeDna of
---             Just dna ->
---                 Ok dna
---             Nothing ->
---                 Err "X"
+        rnaList : List Char
+        rnaList =
+            List.map convertToRNA dnaList
+
+        containsInvalid =
+            List.any (\n -> n == 'X') rnaList
+
+        containsValid =
+            List.any (\n -> Dict.member n rnaDictionary) rnaList
+    in
+        if containsInvalid then
+            if containsValid then
+                Err 'U'
+            else
+                Err 'X'
+        else
+            rnaList
+                |> List.map String.fromChar
+                |> String.join ""
+                |> Ok
